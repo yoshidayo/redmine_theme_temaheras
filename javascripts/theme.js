df3564e0715a7ef0454d15ac7b8b2666c6c6c6e7
アーキテクトチーム <submit@inte.co.jp>
2013-08-29 11:03:52 +0900

    first commit

---
+++ javascripts/theme.js
@@ -0,0 +1,3 @@
+$(window).load(function(){
+	document.getElementById('footer').innerHTML = document.getElementById('footer').innerHTML.replace('Powered by', '<a href="http://www.ibs.inte.co.jp/library/lib001.html">incident manager</a> Powered by');
+});
