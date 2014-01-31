29a7ae17a9f46b0352c0f7b0277c052693050ad8
アーキテクトチーム <submit@inte.co.jp>
2014-01-31 16:29:14 +0900

    Minor modification

--- javascripts/theme.js
+++ javascripts/theme.js
@@ -1,3 +1,3 @@
 $(window).load(function(){
-	document.getElementById('footer').innerHTML = document.getElementById('footer').innerHTML.replace('Powered by', '<a href="http://www.ibs.inte.co.jp/library/lib001.html" target="blank">incident manager</a> Powered by');
+	document.getElementById('footer').innerHTML = document.getElementById('footer').innerHTML.replace('Powered by', '<a href="http://www.ibs.inte.co.jp/service/library/lib001/" target="blank">incident manager</a> Powered by');
 });
