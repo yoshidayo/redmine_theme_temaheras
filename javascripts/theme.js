315d3419809b8d7d3bc7da80a723c030534e1fc2
アーキテクトチーム <submit@inte.co.jp>
2014-07-30 16:49:44 +0900

    changed theme name
    名称変更、およびそれに伴う修正

--- javascripts/theme.js
+++ javascripts/theme.js
@@ -1,3 +1,3 @@
 $(window).load(function(){
-	document.getElementById('footer').innerHTML = document.getElementById('footer').innerHTML.replace('Powered by', '<a href="http://www.ibs.inte.co.jp/service/library/lib001/" target="blank">incident manager</a> Powered by');
+	document.getElementById('footer').innerHTML = document.getElementById('footer').innerHTML.replace('Powered by', '<a href="http://www.ibs.inte.co.jp/service/library/lib001/" target="blank">temaheras</a> Powered by');
 });
